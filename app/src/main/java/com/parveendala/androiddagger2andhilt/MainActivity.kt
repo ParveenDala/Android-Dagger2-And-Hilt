7722b2077d8d1d5c68e2fae76fa9165258622ed5
Parveen D <dParveen94@gmail.com>
2021-08-19 19:53:07 +0530

    Use Bind and Provide annotation

--- app/src/main/java/com/parveendala/androiddagger2andhilt/MainActivity.kt
+++ app/src/main/java/com/parveendala/androiddagger2andhilt/MainActivity.kt
@@ -2,30 +2,54 @@ package com.parveendala.androiddagger2andhilt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject //Field Injection
-    lateinit var randomNumberClass: RandomNumberClass
+    @Inject
+    lateinit var stringConvertor: StringConvertorInterface
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println(randomNumberClass.getRandomNumber())
-        println(randomNumberClass.getRandomText())
+        println(stringConvertor.convertString())
     }
 }
 
-class RandomNumberClass @Inject constructor(
-    private val randomTextClass: RandomTextClass //Constructor Injection
-) {
-    fun getRandomNumber() = "Getting some random number"
-    fun getRandomText() = randomTextClass.getRandomText()
+interface StringConvertorInterface {
+    fun convertString(): String
 }
 
-class RandomTextClass @Inject constructor() {
-    fun getRandomText() = "Getting some random text"
+class StringConvertorImpl @Inject constructor(
+    private val string: String
+) : StringConvertorInterface {
+    override fun convertString(): String {
+        return string.toUpperCase()
+    }
 }
+
+@InstallIn(ActivityComponent::class)
+@Module
+class MyModule {
+
+    @ActivityScoped
+    @Provides
+    fun provideSomeStringToConvert() = "Some string to convert"
+}
+
+@InstallIn(ActivityComponent::class)
+@Module
+abstract class MyModuleToBind {
+    @ActivityScoped
+    @Binds
+    abstract fun bindsStringConvertorImpl(impl: StringConvertorImpl): StringConvertorInterface
+}
+
