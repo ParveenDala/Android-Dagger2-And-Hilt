812fc9b4caeb1d285f1941d6dcc68c5f77361232
Parveen D <dParveen94@gmail.com>
2021-08-19 19:13:08 +0530

    Add Activity with field and constructor injection

--- app/src/main/java/com/parveendala/androiddagger2andhilt/MainActivity.kt
+++ app/src/main/java/com/parveendala/androiddagger2andhilt/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.parveendala.androiddagger2andhilt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject //Field Injection
+    lateinit var randomNumberClass: RandomNumberClass
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(randomNumberClass.getRandomNumber())
+        println(randomNumberClass.getRandomText())
     }
+}
+
+class RandomNumberClass @Inject constructor(
+    private val randomTextClass: RandomTextClass //Constructor Injection
+) {
+    fun getRandomNumber() = "Getting some random number"
+    fun getRandomText() = randomTextClass.getRandomText()
+}
+
+class RandomTextClass @Inject constructor() {
+    fun getRandomText() = "Getting some random text"
 }
